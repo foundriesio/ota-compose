6c69b28cf8ac63ce5f9de8e86841db78d52995ad
Andy Doan <andy@foundries.io>
2019-01-28 15:10:29 -0600

    notary: Add script for initializing notary
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ notary-initialize.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -e
+
+this=$(readlink -f $0)
+data="${NOTARY_DATA-$(dirname $this)/notary-data}"
+collection="${NOTARY_COLLECTION-hub.foundries.io/lmp}"
+
+[ -d $data ] || mkdir $data
+
+if ! which garage-push >/dev/null 2>&1; then
+	echo "Calling via docker with proper tooling"
+	exec docker run -it --rm \
+		--network ota-compose_default \
+		-v $this:$this \
+		-v $(dirname $this)/notary/fixtures:/fixtures \
+		-v $data:$data \
+		--workdir $data \
+		-e NOTARY_DATA=$data \
+		-e NOTARY_COLLECTION=$collection \
+		hub.foundries.io/aktualizr $this
+fi
+
+if [ ! -f $data/notary ] ; then
+	echo "=$(date) Downloading the notary client"
+	wget -O $data/notary https://github.com/theupdateframework/notary/releases/download/v0.6.1/notary-Linux-amd64
+	chmod +x $data/notary
+fi
+
+$data/notary -v -d $data/trust --tlscacert /fixtures/root-ca.crt -s https://notary-server \
+	init $collection
