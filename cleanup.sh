ce18be70db4da1c6ff803ccca775519a24f61eb4
Andy Doan <andy@foundries.io>
2019-04-24 10:22:26 -0500

    cleanup.sh: Remove .secrets
    Signed-off-by: Andy Doan <andy@foundries.io>

--- cleanup.sh
+++ cleanup.sh
@@ -6,3 +6,5 @@ docker-compose -f docker-compose.yml -f uptane.yml down
 docker volume rm ota-compose_ota-ce-db
 docker volume rm ota-compose_treehub-objects
 docker volume rm ota-compose_tuf-targets
+
+rm -f .secrets
