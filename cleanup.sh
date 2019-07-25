15a01a86cf50796a34330ade769017753f918e60
Andy Doan <andy@foundries.io>
2019-07-25 13:47:32 -0500

    Make volumes be in local directory
    Just makes things easier to see for someone not familiar with compose
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- cleanup.sh
+++ cleanup.sh
@@ -3,8 +3,5 @@
 cd $(dirname $(readlink -f $0))
 
 docker-compose -p otacompose -f docker-compose.yml -f uptane.yml down
-docker volume rm otacompose_ota-ce-db
-docker volume rm otacompose_treehub-objects
-docker volume rm otacompose_tuf-targets
-
+rm -rf data/* creds-data
 rm -f .secrets
